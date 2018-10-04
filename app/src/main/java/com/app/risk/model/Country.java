c6eaf06fc1fe2322423b91267152f3a5e498ead6
akshitaangara <35636606+akshitaangara@users.noreply.github.com>
2018-10-04 14:59:50 -0400

    Created continent and country model classes

---
+++ app/src/main/java/com/app/risk/model/Country.java
@@ -0,0 +1,43 @@
+package com.app.risk.model;
+
+/**
+ * Country model to save details of each country
+ * @author Akshita Angara
+ */
+public class Country {
+
+    String nameOfCountry;
+    Continent belongsToContinent;
+
+    /**
+     * Getter function to get name of country
+     * @return name of country
+     */
+    public String getNameOfCountry() {
+        return nameOfCountry;
+    }
+
+    /**
+     * Setter function to set name of country
+     * @param nameOfCountry
+     */
+    public void setNameOfCountry(String nameOfCountry) {
+        this.nameOfCountry = nameOfCountry;
+    }
+
+    /**
+     * Getter function to return the continent that the country belongs to
+     * @return object of Continent that the country belongs to
+     */
+    public Continent getBelongsToContinent() {
+        return belongsToContinent;
+    }
+
+    /**
+     * Setter function to set details of the continent to which the country belongs
+     * @param belongsToContinent
+     */
+    public void setBelongsToContinent(Continent belongsToContinent) {
+        this.belongsToContinent = belongsToContinent;
+    }
+}
