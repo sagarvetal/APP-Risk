53d70d3855dd15b4c9ac8593d74bc5d10770cf8b
akhilachilukuri1 <akhilachilukuri1@gmail.com>
2018-10-05 02:52:47 -0400

    Application Launch

--- app/src/main/java/com/app/risk/MainActivity.java
+++ app/src/main/java/com/app/risk/MainActivity.java
@@ -1,7 +1,10 @@
 package com.app.risk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button createUserMap = (Button) findViewById(R.id.createUserMap);
+        createUserMap.setOnClickListener( new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent userMapCreate = new Intent(MainActivity.this, UserDrivenMaps.class);
+                startActivity(userMapCreate);
+            }
+        });
     }
 }
