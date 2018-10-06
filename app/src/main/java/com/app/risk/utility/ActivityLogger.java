a4eec3dc7b8ded32ff48e5d12bd8e1125a573c5a
Sagar Vetal <vetal.sagar07@gmail.com>
2018-10-06 17:42:30 -0400

    This class logs all activities done during game play.

---
+++ app/src/main/java/com/app/risk/utility/ActivityLogger.java
@@ -0,0 +1,53 @@
+package com.app.risk.utility;
+
+import android.content.Context;
+import android.os.Environment;
+
+import com.app.risk.constants.FileConstants;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * This class logs the all activities done by player
+ * @author Sagar Vetal
+ * @date 06/10/2018
+ * @version 1.0.0
+ */
+public class ActivityLogger {
+
+    /**
+     * This method logs the all activities done by player
+     * @param activity An activity performed by player.
+     */
+    public static void log(final String activity) {
+        try {
+            final FileWriter fileWriter = new FileWriter(getFilePath(),true);
+            final BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(activity);
+            bufferedWriter.newLine();
+            fileWriter.close();
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * This method finds the path of log file and return file object.
+     * If log folder is not present, it will create the log folder.
+     * @return file object
+     */
+    public static File getFilePath() {
+        final String rootPath = Environment.getExternalStorageDirectory().toString();
+        final File logDir = new File(rootPath + File.separator + FileConstants.LOG_FILE_PATH);
+        if (!logDir.exists()) {
+            logDir.mkdirs();
+        }
+        return new File(logDir, FileConstants.LOG_FILE_NAME);
+    }
+}
