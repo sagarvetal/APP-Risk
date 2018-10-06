4bc11988856eac5c01ef1ae40428c8d94bf097b9
Sagar Vetal <vetal.sagar07@gmail.com>
2018-10-06 17:40:34 -0400

    Add class for file constants like file name and path.

---
+++ app/src/main/java/com/app/risk/constants/FileConstants.java
@@ -0,0 +1,14 @@
+package com.app.risk.constants;
+
+/**
+ * This class contains all file constant values
+ * Like name and path of log files and map files, etc.
+ * @author Sagar Vetal
+ * @date 06/10/2018
+ * @version 1.0.0
+ */
+public class FileConstants {
+
+    public static final String LOG_FILE_PATH = "logs";
+    public static final String LOG_FILE_NAME = "logs.txt";
+}
