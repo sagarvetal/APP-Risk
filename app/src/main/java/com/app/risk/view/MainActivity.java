b4a7f57cf2cd72b091d14f1b2f209908631411fe
Himanshu Kohli <atomicboy2012@gmail.com>
2018-10-08 18:49:38 -0400

    add string resources, recyclerview adapters and update mainactivity.java

---
+++ app/src/main/java/com/app/risk/view/MainActivity.java
@@ -0,0 +1,27 @@
+package com.app.risk.view;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.app.risk.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+
+
+        startActivity(new Intent(MainActivity.this,MainScreen.class));
+    }
+
+
+
+
+
+}
