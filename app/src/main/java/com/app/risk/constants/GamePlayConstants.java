fbe5d35cacf41f3639017d18cb405bd6ef229394
Sagar Vetal <vetal.sagar07@gmail.com>
2018-10-10 14:53:26 -0400

    Add file for constants required in game play.
    For example: ARMIES_MULTIPLIER for initial armies assignment.

---
+++ app/src/main/java/com/app/risk/constants/GamePlayConstants.java
@@ -0,0 +1,13 @@
+package com.app.risk.constants;
+
+/**
+ * This class contains all game play constant values
+ * Like armies multiplier, etc.
+ * @author Sagar Vetal
+ * @date 08/10/2018
+ * @version 1.0.0
+ */
+public class GamePlayConstants {
+
+    public static final int AMRIES_MULTIPLIER = 3;
+}
