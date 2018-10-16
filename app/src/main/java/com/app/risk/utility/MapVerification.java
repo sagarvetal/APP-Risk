3322296e32ac9f95835f654ae28feff599c61ce5
akshitaangara <35636606+akshitaangara@users.noreply.github.com>
2018-10-16 18:58:28 -0400

    Modified map verification based on changed GameMap model

--- app/src/main/java/com/app/risk/utility/MapVerification.java
+++ app/src/main/java/com/app/risk/utility/MapVerification.java
@@ -8,21 +8,24 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
+import java.util.Stack;
 
 /**
  * Verify if the map created by the user is correct and follows all the rules and regulations
  * 1. Map is a connected graph.
  * 2. Each continent is a connected sub-graph
  * 3. Each country belongs to one and only one continent
- * 4. All countries are unique and no duplicate countries exist
+ * 4. All countries are unique and no duplicate countries exist in the map
  * @author Akshita Angara
  * @version 1.0.0
  */
 public class MapVerification {
 
-    private List<Country> countryList = new ArrayList<>();
     private List<GameMap> gameMapList = new ArrayList<>();
-    private HashMap<String, String> countryContinentMapping = new HashMap<>();
+    private HashMap<Object, Object> mappingForVerification = new HashMap<>();
+    private Stack<GameMap> depthFirstTraversalStack = new Stack<>();
+
+    private HashMap<Continent, List<Country>> continentCountryMapping = new HashMap<>();
     private List<String> countriesVisited = new ArrayList<>();
 
     /**
@@ -55,65 +58,22 @@ public class MapVerification {
     }
 
     /**
-     * Performs a check to make sure the entire map is a connected graph (implements DFS on the entire graph)
-     * @return true if the map is connected, false otherwise
+     * Performs a check to make sure that all countries are unique and no country is duplicated.
+     * @return true if there are no duplicate countries, false otherwise
      */
-    private boolean checkMapIsConnectedGraph() {
-
-        countriesVisited = null;
-        Set<String> allCountriesInMap = countryContinentMapping.keySet();
-
-        DepthFirstTraversal(gameMapList.get(0));
-
-        for(String country: allCountriesInMap) {
-            if(!countriesVisited.contains(country))
-                return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * Method to perform DFS
-     * @param gameMap
-     */
-    private void DepthFirstTraversal(GameMap gameMap) {
-
-        countriesVisited.add(gameMap.getFromCountry().getNameOfCountry());
-
-        for (Country country: gameMap.getConnectedToCountries()) {
-            if (!countriesVisited.contains(country.getNameOfCountry())) {
-                DepthFirstTraversal(getGameMapObjectByCountry(country));
-            }
-        }
-    }
-
-    /**
-     * Method to return an object of GameMap class based on the from country
-     * @param country
-     * @return GameMap object if the from country equals the parameter
-     */
-    private GameMap getGameMapObjectByCountry(Country country) {
+    private boolean uniqueCountries() {
 
         for (GameMap gameMap: gameMapList) {
-            if (gameMap.getFromCountry().equals(country))
-                return gameMap;
+
+            if(!mappingForVerification.isEmpty() && mappingForVerification.containsKey(gameMap.getFromCountry().getNameOfCountry())){
+                System.out.println(mappingForVerification.containsKey(gameMap.getFromCountry().getNameOfCountry()) + " already exists.");
+                return false;
+            } else {
+                mappingForVerification.put(gameMap.getFromCountry().getNameOfCountry(), gameMap.getFromCountry());
+            }
         }
 
-        return null;
-    }
-
-    /**
-     * Performs a check to make sure each continent in the map is a connected subgraph
-     * (implements DFS on each continent)
-     * @return true if each continent is a connected subgraph, false otherwise
-     */
-    private boolean checkContinentIsConnectedSubgraph() {
-
-        List<Country> countryList = new ArrayList<>();
-
-
-
+        mappingForVerification = null;
         return true;
     }
 
@@ -125,40 +85,87 @@ public class MapVerification {
 
         for(GameMap gameMap: gameMapList) {
 
-            if(countryContinentMapping.containsKey(gameMap.getFromCountry().getNameOfCountry())) {
-
-                if(!countryContinentMapping.get(gameMap.getFromCountry().getNameOfCountry())
-                        .equals(gameMap.getFromCountry().getBelongsToContinent().getNameOfContinent())) {
-
-                    System.out.println(countryContinentMapping.get(gameMap.getFromCountry().getNameOfCountry()) +
-                            " belongs to more than one continent.");
-                    return false;
-                }
+            if(!mappingForVerification.isEmpty() && mappingForVerification.containsKey(gameMap.getFromCountry())
+                    && !mappingForVerification.get(gameMap.getFromCountry()).equals(gameMap.getFromCountry().getBelongsToContinent())) {
+                System.out.println(mappingForVerification.get(gameMap.getFromCountry().getNameOfCountry()) + " belongs to more than one continent.");
+                return false;
             } else {
-                countryContinentMapping.put(gameMap.getFromCountry().getNameOfCountry(),
-                        gameMap.getFromCountry().getBelongsToContinent().getNameOfContinent());
+                mappingForVerification.put(gameMap.getFromCountry(), gameMap.getFromCountry().getBelongsToContinent());
             }
+        }
+
+        mappingForVerification = null;
+        return true;
+    }
+
+    /**
+     * Performs a check to make sure the entire map is a connected graph
+     * (implements DFS on the entire graph)
+     * @return true if the map is connected, false otherwise
+     */
+    private boolean checkMapIsConnectedGraph() {
+
+        countriesVisited = null;
+        depthFirstTraversalStack.push(gameMapList.get(0));
+
+        DepthFirstTraversal();
+
+        for(GameMap gameMap: gameMapList) {
+            if(!countriesVisited.contains(gameMap.getFromCountry().getNameOfCountry()))
+                return false;
         }
 
         return true;
     }
 
     /**
-     * Performs a check to make sure that all countries are unique and no country is duplicated.
-     * @return true if there are no duplicate countries, false otherwise
+     * Performs a check to make sure each continent in the map is a connected subgraph
+     * (implements DFS on each continent)
+     * @return true if each continent is a connected subgraph, false otherwise
      */
-    private boolean uniqueCountries() {
+    private boolean checkContinentIsConnectedSubgraph() {
 
-        for (GameMap gameMap: gameMapList) {
-
-            if (!countryList.isEmpty() && countryList.contains(gameMap.getFromCountry())) {
-                System.out.println(gameMap.getFromCountry().getNameOfCountry() + " already exists.");
-                return false;
-            } else {
-                countryList.add(gameMap.getFromCountry());
-            }
-        }
 
         return true;
+    }
+
+    /**
+     * Method to perform DFS
+     */
+    private void DepthFirstTraversal() {
+
+        while (!depthFirstTraversalStack.empty()){
+
+            GameMap countryVisited = depthFirstTraversalStack.pop();
+
+            if(!countriesVisited.isEmpty() && countriesVisited.contains(countryVisited.getFromCountry().getNameOfCountry())){
+                continue;
+            } else {
+                countriesVisited.add(countryVisited.getFromCountry().getNameOfCountry());
+
+                for (GameMap neighbourCountry: countryVisited.getConnectedToCountries()){
+                    if(!countriesVisited.contains(neighbourCountry.getFromCountry().getNameOfCountry())){
+                        depthFirstTraversalStack.push(neighbourCountry);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Method to generate a hashmap with continent as key and a list of countries belonging to that continent as its value
+     */
+    private void generateContinentCountryMapping() {
+
+        for (GameMap gameMap: gameMapList) {
+            if (continentCountryMapping.containsKey(gameMap.getFromCountry().getBelongsToContinent())) {
+                continentCountryMapping.get(gameMap.getFromCountry().getBelongsToContinent())
+                        .add(gameMap.getFromCountry());
+            } else {
+                List<Country> country = new ArrayList<>();
+                country.add(gameMap.getFromCountry());
+                continentCountryMapping.put(gameMap.getFromCountry().getBelongsToContinent(), country);
+            }
+        }
     }
 }
