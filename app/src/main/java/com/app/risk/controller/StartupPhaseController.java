adc8275ce1758da2423c5db2b7ea5c1aba7fff1b
Sagar Vetal <vetal.sagar07@gmail.com>
2018-10-16 14:30:25 -0400

    Place armies on countries in round-robbin fashion.
    First place one one army to each country and
    then remaing armies which players are having, are placed in
    round-robbin fashion.
    
    Also change the logic of assigning initial armies to each player.

--- app/src/main/java/com/app/risk/controller/StartupPhaseController.java
+++ app/src/main/java/com/app/risk/controller/StartupPhaseController.java
@@ -1,13 +1,15 @@
 package com.app.risk.controller;
 
 import com.app.risk.constants.GamePlayConstants;
+import com.app.risk.model.Country;
 import com.app.risk.model.GamePlay;
 import com.app.risk.model.Player;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * This class is used to randomly allocate initial armies to player and
@@ -48,20 +50,73 @@ public class StartupPhaseController {
      * @param gamePlay The gamePlay object.
      */
     public void assignInitialArmies(final GamePlay gamePlay) {
-        final int totalArmies = gamePlay.getCountries().size() * GamePlayConstants.AMRIES_MULTIPLIER;
-        final int armiesPerPlayer = totalArmies / gamePlay.getPlayers().size();
-
         for(final Player player : gamePlay.getPlayers().values()) {
-            player.setNoOfArmies(armiesPerPlayer);
+            player.setNoOfArmies(player.getNoOfCountries() * GamePlayConstants.AMRIES_MULTIPLIER);
+        }
+    }
+
+    /**
+     * This method place initial armies on countries in round robbin fashion,
+     * @param gamePlay The gamePlay object.
+     */
+    public void placeInitialArmies(final GamePlay gamePlay){
+
+        /*Place one army on each country*/
+        for(final Country country : gamePlay.getCountries().values()) {
+            country.incrementArmies(1);
         }
 
-        int remainingArmies = totalArmies % gamePlay.getPlayers().size();
-        if(remainingArmies != 0) {
-            final List<Integer> playerIds =  new ArrayList<>(gamePlay.getPlayers().keySet());
-            for(int i = 0; remainingArmies != 0; i++, remainingArmies--) {
-                gamePlay.getPlayers().get(playerIds.get(i)).incrementArmies(1);
+        boolean isMoreArmiesAvailable = false;
+        for(final Player player : gamePlay.getPlayers().values()) {
+            final int diff = player.getNoOfArmies() - player.getNoOfCountries();
+            if(diff > 0) {
+                isMoreArmiesAvailable = true;
+                break;
             }
         }
+
+        /*
+         * If players are having more no of armies than no of countries,
+         * then place the armies in round robbin fashion.
+         */
+        if(isMoreArmiesAvailable) {
+            final HashMap<Integer, Integer> hmPlayerUnplacedArmies = new HashMap<>();
+            final HashMap<Integer, List<Country>> hmPlayerCountries= new HashMap<>();
+            int totalUnplacedArmies = 0;
+
+            for(final Player player : gamePlay.getPlayers().values()) {
+                hmPlayerUnplacedArmies.put(player.getId(), player.getNoOfArmies() - player.getNoOfCountries());
+                hmPlayerCountries.put(player.getId(), getCountryListByPlayerId(player.getId(), gamePlay));
+                totalUnplacedArmies += player.getNoOfArmies() - player.getNoOfCountries();
+            }
+
+            while(totalUnplacedArmies != 0) {
+                for(final Player player : gamePlay.getPlayers().values()) {
+                    if(hmPlayerUnplacedArmies.get(player.getId()) > 0){
+                        final List<Country> countryList = hmPlayerCountries.get(player.getId());
+                        final int randomIndex = ThreadLocalRandom.current().nextInt(countryList.size());
+                        countryList.get(randomIndex).incrementArmies(1);
+                        hmPlayerUnplacedArmies.put(player.getId(), hmPlayerUnplacedArmies.get(player.getId()) - 1);
+                        totalUnplacedArmies -= 1;
+                    }
+                }
+            }
+        }
+
+    }
+
+    /**
+     * This method gives list of countries concurred by given player.
+     * @param playerId This is player id.
+     */
+    public List<Country> getCountryListByPlayerId(final int playerId, final GamePlay gamePlay) {
+        final List<Country> countryList = new ArrayList<>();
+        for(final Country country : gamePlay.getCountries().values()) {
+            if(country.getPlayer().getId() == playerId) {
+                countryList.add(country);
+            }
+        }
+        return countryList;
     }
 
 }
