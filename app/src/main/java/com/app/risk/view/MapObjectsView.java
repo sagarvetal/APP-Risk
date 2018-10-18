d53636ed9c0879248b6469faa8881a7ede4d23e8
akhilachilukuri1 <akhilachilukuri1@gmail.com>
2018-10-17 22:11:51 -0400

    map object view

---
+++ app/src/main/java/com/app/risk/view/MapObjectsView.java
@@ -0,0 +1,28 @@
+package com.app.risk.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+public class MapObjectsView extends View {
+    private Paint paint;
+    public float xPosition;
+    public float yPosition;
+    public  int continentColor;
+
+
+    public MapObjectsView(Context context) {
+        super(context);
+        // create the Paint and set its color
+        paint = new Paint();
+        paint.setColor(continentColor);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.BLUE);
+        canvas.drawCircle(100, 100, 100, paint);
+    }
+}
