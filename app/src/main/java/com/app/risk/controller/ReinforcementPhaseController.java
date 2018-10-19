1765ca7ce7c89904c046e7e066d4a284eec88edd
Sagar Vetal <vetal.sagar07@gmail.com>
2018-10-19 01:06:47 -0400

    Refactor Code.
    Move getCountriesByPlayerId() method into GamePlay model and
    its respective changes.

--- app/src/main/java/com/app/risk/controller/ReinforcementPhaseController.java
+++ app/src/main/java/com/app/risk/controller/ReinforcementPhaseController.java
@@ -27,37 +27,23 @@ public class ReinforcementPhaseController {
     /**
      * This method sets the no of reinforcement armies given to player
      * based on no of countries player owns.
-     * @param countriesOwnedByPlayer The list of countries owned by  player.
      */
-    public void setReinforcementArmies(final ArrayList<Country> countriesOwnedByPlayer) {
-        final int reinforcementArmies = calculateReinforcementArmies(countriesOwnedByPlayer);
+    public void setReinforcementArmies() {
+        final int reinforcementArmies = calculateReinforcementArmies();
         gamePlay.getCurrentPlayer().setReinforcementArmies(reinforcementArmies);
         gamePlay.getCurrentPlayer().incrementArmies(reinforcementArmies);
     }
 
     /**
      * This method calculates the no of reinforcement armies.
-     * @param countriesOwnedByPlayer The list of countries owned by  player.
      */
-    public int calculateReinforcementArmies(final ArrayList<Country> countriesOwnedByPlayer){
-        int reinforcementArmies = countriesOwnedByPlayer.size() / 3;
+    public int calculateReinforcementArmies(){
+        final ArrayList<Country> countriesOwnedByPlayer = gamePlay.getCountryListByPlayerId(gamePlay.getCurrentPlayer().getId());
+        final int reinforcementArmies = countriesOwnedByPlayer.size() / 3;
         if(reinforcementArmies > GamePlayConstants.MIN_REINFORCEMENT_AMRIES) {
             return  reinforcementArmies;
         }
         return GamePlayConstants.MIN_REINFORCEMENT_AMRIES;
     }
 
-    /**
-     * This method gives list of countries concurred by given player.
-     * @param playerId This is player id.
-     */
-    public ArrayList<Country> getCountryListByPlayerId(final int playerId) {
-        final ArrayList<Country> countryList = new ArrayList<>();
-        for(final Country country : gamePlay.getCountries().values()) {
-            if(country.getPlayer().getId() == playerId) {
-                countryList.add(country);
-            }
-        }
-        return countryList;
-    }
 }
