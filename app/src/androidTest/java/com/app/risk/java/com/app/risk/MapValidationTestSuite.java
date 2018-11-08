852638cc134d6ae5ac57521a4dc44733fbcc155c
Urvi <u_l@encs.concordia.ca>
2018-11-08 17:12:19 -0500

    Test cases

--- app/src/androidTest/java/com/app/risk/java/com/app/risk/MapValidationTestSuite.java
+++ app/src/androidTest/java/com/app/risk/java/com/app/risk/MapValidationTestSuite.java
@@ -1,20 +1,7 @@
 package com.app.risk.java.com.app.risk;
 
-import com.app.risk.java.com.app.risk.utility.ConnectedGraphTest;
-import com.app.risk.java.com.app.risk.utility.DisConnectedGraphTest;
-import com.app.risk.java.com.app.risk.utility.FortificationConnectedTest;
-import com.app.risk.java.com.app.risk.utility.FortificationUnConnectedTest;
-import com.app.risk.java.com.app.risk.utility.InvalidMapTest;
-import com.app.risk.java.com.app.risk.utility.MapReaderTest;
-import com.app.risk.java.com.app.risk.utility.OneContinentCheckTest;
-import com.app.risk.java.com.app.risk.utility.ReinforcementArmiesCountTest;
-import com.app.risk.java.com.app.risk.utility.SaveMapTest;
-import com.app.risk.java.com.app.risk.utility.StartUpPhaseAssignArmiesTest;
-import com.app.risk.java.com.app.risk.utility.StartUpPhaseAssignCountriesTest;
-import com.app.risk.java.com.app.risk.utility.StartUpPhaseInitialArmies;
-import com.app.risk.java.com.app.risk.utility.SubGraphConnectedTest;
-import com.app.risk.java.com.app.risk.utility.UniqueCountryTest;
-import com.app.risk.java.com.app.risk.utility.ValidConnectedGraphTest;
+import com.app.risk.java.com.app.risk.utility.ContinentsOwnedByPlayerTest;
+import com.app.risk.java.com.app.risk.utility.PercentageOfMapOwnedByPlayerTest;
 
 
 import org.junit.runner.RunWith;
@@ -31,21 +18,9 @@ import org.junit.runners.Suite;
  * Suite for Map Validation
  */
 @Suite.SuiteClasses({
-        ConnectedGraphTest.class,           //Validation for connected graph
-        MapReaderTest.class,                //Validation for reading the Map
-        OneContinentCheckTest.class,        //Checks for one continent countries
-        SubGraphConnectedTest.class,        //Checks whether subgraph is connected in graph
-        UniqueCountryTest.class,           //Checks whether countries are unique or not
-        SaveMapTest.class,                 //validates whther the map is saved or not
-        DisConnectedGraphTest.class,
-        ValidConnectedGraphTest.class,
-        InvalidMapTest.class,
-        FortificationConnectedTest.class,
-        FortificationUnConnectedTest.class,
-        ReinforcementArmiesCountTest.class,
-        StartUpPhaseAssignArmiesTest.class,
-        StartUpPhaseAssignCountriesTest.class,
-        StartUpPhaseInitialArmies.class,
+
+        PercentageOfMapOwnedByPlayerTest.class,
+        ContinentsOwnedByPlayerTest.class
 })
 public class MapValidationTestSuite {
 }
