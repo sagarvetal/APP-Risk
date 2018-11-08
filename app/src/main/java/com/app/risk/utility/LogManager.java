9140c89cff66c44e3c9a767c7758b077fc81ce6f
akhilachilukuri1 <akhilachilukuri1@gmail.com>
2018-11-08 11:49:47 -0500

    Log manager comments

--- app/src/main/java/com/app/risk/utility/LogManager.java
+++ app/src/main/java/com/app/risk/utility/LogManager.java
@@ -14,32 +14,44 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+/**
+ * This class manages the operate on the logs
+ *like reading,writing and deleting the log
+ * @author Akhila Chilukuri
+ * @version 1.0.0
+ */
 
 public class LogManager {
     private static LogManager logManager;
     private String dirPath;
     public static Log logmsg;
+    /**
+     * This is a default constructor.
+     *
+     */
     private LogManager() {
     }
-    public static LogManager getInstance() {
+
+    /**
+     * This is a method that returns the same single instance of the LogManager class
+     * when it is called
+     * @param dirPath path in which the file is generated with all the logs
+     * @param view on which the observer pattern is implemented
+     * @return LogManager single instance of the class
+     */
+    public static LogManager getInstance(String dirPath, PlayScreenActivity view) {
         if (logManager == null) {
             logManager = new LogManager();
-        }
-        return logManager;
-    }
-    public static LogManager getInstance(String dirPath,PlayScreenActivity view) {
-        if (logManager == null) {
-            logManager = new LogManager();
-            logManager.dirPath=dirPath;
-            logmsg=new Log();
+            logManager.dirPath = dirPath;
+            logmsg = new Log();
             logmsg.addObserver(view);
-       /*     File logDirectory = new File(dirPath);
-            if (!logDirectory.exists())
-                logDirectory.mkdirs();
-            File file = new File(logDirectory, FileConstants.LOG_FILE_NAME);*/
         }
         return logManager;
     }
+    /**
+     * This is a method that writes the given log in the file and log object
+     * @param text message of the log that would be written into file and displayed on the view
+     */
     public void writeLog(String text) {
         BufferedWriter output = null;
         try {
@@ -48,29 +60,25 @@ public class LogManager {
             if (!logDirectory.exists())
                 logDirectory.mkdirs();
             File file = new File(logDirectory, FileConstants.LOG_FILE_NAME);
-            boolean result=false;
+            boolean result = false;
             try {
 
-               result=file.createNewFile();
-                System.out.println("::::::::::::::::::::::::::::::::write LOg result::::::::::::::::::"+result);
+                result = file.createNewFile();
+                System.out.println("::::::::::::::::::::::::::::::::write LOg result::::::::::::::::::" + result);
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
-                FileOutputStream f = new FileOutputStream(file,true);
-           // FileOutputStream f = new FileOutputStream(file);
+            FileOutputStream f = new FileOutputStream(file, true);
             output = new BufferedWriter(new OutputStreamWriter(f));
-                output.write(text);
-                output.newLine();
+            output.write(text);
+            output.newLine();
 
         } catch (IOException e) {
 
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
 
-        }
-        finally {
+        } finally {
             if (output != null) {
                 try {
                     output.close();
@@ -79,14 +87,14 @@ public class LogManager {
                 }
 
                 logmsg.setMessage(text);
-                //PlayScreenActivity.logViewArrayList.add(0,text);
-               // PlayScreenActivity.logViewAdapter.notifyDataSetChanged();
-               // PhaseController.getInstance().update();
             }
         }
     }
 
-
+    /**
+     * This is a method that reads the logs from the file
+     * @return List<String> list of the messages that have been logged into the file
+     */
     public List<String> readLog() {
         Scanner scanner = null;
         ArrayList<String> logs = new ArrayList<String>();
@@ -95,8 +103,8 @@ public class LogManager {
             logDirectory.mkdirs();
         File file = new File(logDirectory, FileConstants.LOG_FILE_NAME);
         try {
-            boolean result=file.createNewFile();
-            System.out.println("::::::::::::::::::::::::::::::::read LOG  result::::::::::::::::::"+result);
+            boolean result = file.createNewFile();
+            System.out.println("::::::::::::::::::::::::::::::::read LOG  result::::::::::::::::::" + result);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -112,18 +120,32 @@ public class LogManager {
         return logs;
     }
 
-
+    /**
+     * This is a method that delete the log from the file
+     * and the clear the log from the UI
+     */
     public void deleteLog() {
         File logDirectory = new File(LogManager.getInstance().dirPath);
         if (!logDirectory.exists())
             logDirectory.mkdirs();
         File file = new File(logDirectory, FileConstants.LOG_FILE_NAME);
         if (file.exists()) {
-            boolean result=file.delete();
-            System.out.println(":::::::::::::::::::::::::delete result::::::::::::::"+result);
+            boolean result = file.delete();
+            System.out.println(":::::::::::::::::::::::::delete result::::::::::::::" + result);
             PlayScreenActivity.logViewArrayList.removeAll(PlayScreenActivity.logViewArrayList);
             PlayScreenActivity.logViewAdapter.notifyDataSetChanged();
         }
-       // PhaseController.getInstance().deleteUpdate();
+    }
+
+    /**
+     * This is a method that returns the same single instance of the LogManager class
+     * when it is called
+     * @return LogManager single instance of the class
+     */
+    public static LogManager getInstance() {
+        if (logManager == null) {
+            logManager = new LogManager();
+        }
+        return logManager;
     }
 }
