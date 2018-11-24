3504d6144696bd2d22e39c15faba513bb1aa58e3
Sagar Vetal <vetal.sagar07@gmail.com>
2018-11-24 00:55:48 -0500

    Add implementation class for random player strategy.

---
+++ app/src/main/java/com/app/risk/model/RandomPlayerStrategy.java
@@ -0,0 +1,46 @@
+package com.app.risk.model;
+
+import com.app.risk.Interfaces.Strategy;
+
+/**
+ * A concrete strategy class that implements a random player strategy.
+ *
+ * @author Sagar Vetal
+ * @version 1.0.0 (Date: 22/11/2018)
+ */
+public class RandomPlayerStrategy implements Strategy {
+
+    /**
+     * This is reinforcement method for random strategy player.
+     * It places the random no of reinforcement armies on random country player owns.
+     * @param gamePlay The GamePlay object.
+     * @param player The Player object.
+     */
+    @Override
+    public void reinforcementPhase(final GamePlay gamePlay, final Player player) {
+
+    }
+
+    /**
+     * This is attack method for random strategy player.
+     * It attacks a random no of times on a random country.
+     * @param gamePlay The GamePlay object.
+     * @param player The Player object.
+     */
+    @Override
+    public void attackPhase(final GamePlay gamePlay, final Player player) {
+
+    }
+
+    /**
+     * This is fortification method for random strategy player.
+     * It fortifies a random country.
+     * @param gamePlay The GamePlay object.
+     * @param player The Player object.
+     */
+    @Override
+    public void fortificationPhase(final GamePlay gamePlay, final Player player) {
+
+    }
+
+}
