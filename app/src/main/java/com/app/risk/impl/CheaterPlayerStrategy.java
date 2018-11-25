0830eb7766c51ff32d3f3fc72acfee78631f9093
Himanshu Kohli <atomicboy2012@gmail.com>
2018-11-25 02:04:14 -0500

    Update Cheater and manifest xml

--- app/src/main/java/com/app/risk/impl/CheaterPlayerStrategy.java
+++ app/src/main/java/com/app/risk/impl/CheaterPlayerStrategy.java
@@ -1,8 +1,11 @@
 package com.app.risk.impl;
 
 import com.app.risk.Interfaces.Strategy;
+import com.app.risk.model.Country;
 import com.app.risk.model.GamePlay;
 import com.app.risk.model.Player;
+
+import java.util.ArrayList;
 
 /**
  * A concrete strategy class that implements a cheater player strategy.
@@ -21,6 +24,12 @@ public class CheaterPlayerStrategy implements Strategy {
     @Override
     public void reinforcementPhase(final GamePlay gamePlay, final Player player) {
 
+        ArrayList<Country> countryArrayList = gamePlay.getCountryListByPlayerId(player.getId());
+
+        for(Country country : countryArrayList){
+            country.setNoOfArmies(country.getNoOfArmies() * 2);
+        }
+        player.setNoOfArmies(player.getNoOfArmies() * 2);
     }
 
     /**
@@ -31,7 +40,18 @@ public class CheaterPlayerStrategy implements Strategy {
      */
     @Override
     public void attackPhase(final GamePlay gamePlay, final Player player) {
+        ArrayList<Country> countryArrayList = gamePlay.getCountryListByPlayerId(player.getId());
 
+        for(Country country: countryArrayList){
+            ArrayList<String> adjacentCountries = country.getAdjacentCountries();
+            for(String neighbour: adjacentCountries){
+                Country defenderCountry = gamePlay.getCountries().get(neighbour);
+                int noOfArmies = defenderCountry.getNoOfArmies();
+                defenderCountry.getPlayer().decrementArmies(noOfArmies);
+                defenderCountry.setPlayer(player);
+                defenderCountry.getPlayer().incrementArmies(noOfArmies);
+            }
+        }
     }
 
     /**
@@ -42,7 +62,19 @@ public class CheaterPlayerStrategy implements Strategy {
      */
     @Override
     public void fortificationPhase(final GamePlay gamePlay, final Player player) {
+        ArrayList<Country> countryArrayList = gamePlay.getCountryListByPlayerId(player.getId());
 
+        for(Country country : countryArrayList){
+            ArrayList<String> adjacentCountries = country.getAdjacentCountries();
+            for(String neighbour: adjacentCountries){
+                if(player.getId() != gamePlay.getCountries().get(neighbour).getPlayer().getId()){
+                    int noOfArmies = country.getNoOfArmies();
+                    country.incrementArmies(noOfArmies);
+                    player.incrementArmies(noOfArmies);
+                    break;
+                }
+            }
+        }
     }
 
 }
