fa9c4d941d2c2f96aaa6987e1679095d8b887e63
akshitaangara <35636606+akshitaangara@users.noreply.github.com>
2018-11-24 20:34:30 -0500

    Random player fortification strategy implementation

--- app/src/main/java/com/app/risk/impl/RandomPlayerStrategy.java
+++ app/src/main/java/com/app/risk/impl/RandomPlayerStrategy.java
@@ -1,8 +1,16 @@
 package com.app.risk.impl;
 
+import android.content.Context;
+
 import com.app.risk.Interfaces.Strategy;
+import com.app.risk.controller.FortificationPhaseController;
+import com.app.risk.model.Country;
 import com.app.risk.model.GamePlay;
 import com.app.risk.model.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * A concrete strategy class that implements a random player strategy.
@@ -12,11 +20,14 @@ import com.app.risk.model.Player;
  */
 public class RandomPlayerStrategy implements Strategy {
 
+    private Random random = new Random();
+
     /**
      * This is reinforcement method for random strategy player.
      * It places the random no of reinforcement armies on random country player owns.
+     *
      * @param gamePlay The GamePlay object.
-     * @param player The Player object.
+     * @param player   The Player object.
      */
     @Override
     public void reinforcementPhase(final GamePlay gamePlay, final Player player) {
@@ -26,8 +37,9 @@ public class RandomPlayerStrategy implements Strategy {
     /**
      * This is attack method for random strategy player.
      * It attacks a random no of times on a random country.
+     *
      * @param gamePlay The GamePlay object.
-     * @param player The Player object.
+     * @param player   The Player object.
      */
     @Override
     public void attackPhase(final GamePlay gamePlay, final Player player) {
@@ -37,12 +49,22 @@ public class RandomPlayerStrategy implements Strategy {
     /**
      * This is fortification method for random strategy player.
      * It fortifies a random country.
+     *
      * @param gamePlay The GamePlay object.
-     * @param player The Player object.
+     * @param player   The Player object.
      */
     @Override
     public void fortificationPhase(final GamePlay gamePlay, final Player player) {
-
+        ArrayList<Country> countriesBelongingToPlayer = gamePlay.getCountryListByPlayerId(player.getId());
+        while (true) {
+            int fromCountryIndex = random.nextInt(countriesBelongingToPlayer.size());
+            Country fromCountry = countriesBelongingToPlayer.get(fromCountryIndex);
+            if (fromCountry.getNoOfArmies() > 1) {
+                List<String> reachableCountries = FortificationPhaseController.getInstance().init(null, gamePlay).getReachableCountries(fromCountry, countriesBelongingToPlayer);
+                Country toCountry = gamePlay.getCountries().get(reachableCountries.get(random.nextInt(reachableCountries.size())));
+                FortificationPhaseController.getInstance().init(null, gamePlay).fortifyCountry(fromCountry, toCountry, random.nextInt(fromCountry.getNoOfArmies() - 1));
+                break;
+            }
+        }
     }
-
 }
