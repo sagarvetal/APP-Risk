ec93b72e7cfee362433f6e2725fcced0e81c6a0b
Himanshu Kohli <atomicboy2012@gmail.com>
2018-11-26 01:31:42 -0500

    Update javadocs and finalize views

--- app/src/main/java/com/app/risk/view/TournamentMenuActivity.java
+++ app/src/main/java/com/app/risk/view/TournamentMenuActivity.java
@@ -2,55 +2,73 @@ package com.app.risk.view;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
 import android.view.View;
-import android.widget.ListView;
+import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.Toast;
 
 import com.app.risk.R;
 import com.app.risk.constants.GamePlayConstants;
-import com.app.risk.model.GamePlay;
 
 import java.util.ArrayList;
 
 public class TournamentMenuActivity extends AppCompatActivity implements View.OnClickListener {
 
 
-    private CardView mapSelectionCard,stratergiesSelectionCard,okayCard;
-    private NumberPicker playerRestrictionPicker;
+    private Button mapSelectionButton, stratergiesSelectionButton, okayButton;
+    private NumberPicker playerRestrictionPicker,gameSelectionPicker;
 
     private ArrayList<String> mapArrayList;
     private ArrayList<String> selectedPlayerStratergies,selectedMapList;
-    private String[] playerStratergies = {GamePlayConstants.AGGRESSIVE_STRATEGY,
+    private String[] playerStratergiesArray = {GamePlayConstants.AGGRESSIVE_STRATEGY,
             GamePlayConstants.BENEVOLENT_STRATEGY,GamePlayConstants.RANDOM_STRATEGY,
             GamePlayConstants.CHEATER_STRATEGY};
 
+    private String[] mapListArray;
+    private boolean[] checkedStateMapArray,checkedStateStratergiesArray;
+
+
+    /**
+     * {@inheritDoc}
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tournament_menu);
 
+        getSupportActionBar().setTitle("Tournament Selection");
+
         initializeId();
+        initilizeElements();
 
     }
 
+    /**
+     * This method initialize the Ids and sets listeners to buttons
+     */
     public void initializeId(){
 
-        mapSelectionCard = findViewById(R.id.tournament_menu_map_selection);
-        stratergiesSelectionCard = findViewById(R.id.tournament_menu_player_stratergies);
-        okayCard = findViewById(R.id.tournament_menu_okay_card);
+        mapSelectionButton = findViewById(R.id.tournament_menu_map_selection);
+        stratergiesSelectionButton = findViewById(R.id.tournament_menu_player_stratergies);
+        okayButton = findViewById(R.id.tournament_menu_okay_card);
         playerRestrictionPicker = findViewById(R.id.tournament_menu_number_picker);
         playerRestrictionPicker.setMinValue(10);
         playerRestrictionPicker.setMaxValue(50);
         playerRestrictionPicker.setValue(25);
         playerRestrictionPicker.setWrapSelectorWheel(false);
 
-        mapSelectionCard.setOnClickListener(this);
-        stratergiesSelectionCard.setOnClickListener(this);
-        okayCard.setOnClickListener(this);
+        gameSelectionPicker = findViewById(R.id.tournament_menu_game_number_picker);
+        gameSelectionPicker.setMinValue(1);
+        gameSelectionPicker.setMaxValue(5);
+        gameSelectionPicker.setWrapSelectorWheel(false);
+
+        mapSelectionButton.setOnClickListener(this);
+        stratergiesSelectionButton.setOnClickListener(this);
+        okayButton.setOnClickListener(this);
 
         mapArrayList = new ArrayList<>();
         selectedPlayerStratergies = new ArrayList<>();
@@ -60,68 +78,122 @@ public class TournamentMenuActivity extends AppCompatActivity implements View.On
         }
     }
 
-    AlertDialog.Builder alertDialog;
-    AlertDialog alertDialogMain;
+    /**
+     * This method initialize elements for alert dialogs
+     */
+    public void initilizeElements(){
+        mapListArray = new String[mapArrayList.size()];
+        mapArrayList.toArray(mapListArray);
+        checkedStateMapArray = new boolean[mapArrayList.size()];
 
-    public void createAlertDialog(){
+        for(int i = 0; i< checkedStateMapArray.length; i++){
+            checkedStateMapArray[i] = false;
+        }
 
-        final String[] mapArray = new String[mapArrayList.size()];
-        mapArrayList.toArray(mapArray);
-        Boolean[] checkedState = new Boolean[mapArrayList.size()];
 
-        alertDialog = new AlertDialog.Builder(this);
-                alertDialog.setMultiChoiceItems(mapArray, null, new DialogInterface.OnMultiChoiceClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
-                        if(isChecked){
-                            selectedMapList.add(mapArray[which]);
-                        }
-                        else{
-                            selectedMapList.remove(mapArray[which]);
-                        }
-                    }
-                });
-                alertDialog.setPositiveButton("Ok",null);
-                alertDialog.setTitle("Select Maps");
-                alertDialog.setCancelable(false);
-                alertDialogMain = alertDialog.create();
+        checkedStateStratergiesArray = new boolean[playerStratergiesArray.length];
+        for(int i = 0; i< playerStratergiesArray.length; i++){
+            checkedStateStratergiesArray[i] = false;
+        }
+
     }
 
+
+    /**
+     * {@inheritDoc}
+     * @param v: Invoking view
+     */
     @Override
     public void onClick(View v) {
 
-        if(v == mapSelectionCard){
-            alertDialogMain.show();
-        }
-        if(v == stratergiesSelectionCard){
-
+        if(v == mapSelectionButton){
             new AlertDialog.Builder(this)
-                    .setMultiChoiceItems(playerStratergies, null, new DialogInterface.OnMultiChoiceClickListener() {
+                    .setMultiChoiceItems(mapListArray, checkedStateMapArray, new DialogInterface.OnMultiChoiceClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which, boolean isChecked) {
                             if(isChecked){
-                                selectedPlayerStratergies.add(playerStratergies[which]);
+                                checkedStateMapArray[which] = true;
                             }
                             else{
-                                selectedPlayerStratergies.remove(playerStratergies[which]);
+                                checkedStateMapArray[which] = false;
                             }
                         }
                     })
-                    .setPositiveButton("Ok",null)
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            addElementsToList(checkedStateMapArray,selectedMapList, mapListArray);
+
+                            mapSelectionButton.setText(selectedMapList
+                                    +   " maps selected");
+
+                        }
+                    })
+                    .setTitle("Select Maps")
+                    .setCancelable(false).create().show();
+        }
+        if(v == stratergiesSelectionButton){
+
+            new AlertDialog.Builder(this)
+                    .setMultiChoiceItems(playerStratergiesArray, checkedStateStratergiesArray, new DialogInterface.OnMultiChoiceClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                            if(isChecked){
+                                checkedStateStratergiesArray[which] = true;
+                            }
+                            else{
+                                checkedStateStratergiesArray[which] = false;
+                            }
+                        }
+                    })
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            addElementsToList(checkedStateStratergiesArray
+                            ,selectedPlayerStratergies,playerStratergiesArray);
+
+                            stratergiesSelectionButton.setText(selectedPlayerStratergies
+                            +   " stratergies selected");
+                        }
+                    })
                     .setTitle("Select Stratergies")
                     .setCancelable(false)
                     .create().show();
 
         }
-        if(v == okayCard){
+        if(v == okayButton){
             int numberPickerValue = playerRestrictionPicker.getValue();
+            int gamePickerValue = gameSelectionPicker.getValue();
+            if(selectedPlayerStratergies.size() >=2 && selectedMapList.size() >=1){
+//                Intent intent = new Intent(TournamentMenuActivity.this,MainScreenActivity.class);
+//                intent.putStringArrayListExtra("STRATERGIES",selectedPlayerStratergies);
+//                intent.putStringArrayListExtra("MAPS",selectedMapList);
+//                intent.putExtra("MAX_TURNS",numberPickerValue);
+                // intent.putExtra("GAMES",gamePickerValue);
+                Toast.makeText(this, ""+gamePickerValue, Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(this, "" +
+                        "Please Select at least one stratergy \n" +
+                        "and two player stratergies.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
-            Toast.makeText(this, ""
-                    + numberPickerValue + "\n"
-                    + selectedMapList.toString() + "\n"
-                    + selectedPlayerStratergies.toString()+ "\n", Toast.LENGTH_SHORT).show();
 
+    /**
+     * This method adds elements to the final selected arrayList
+     * @param checkedList : boolean which holds the checked state of multi choice alert dialog
+     * @param arrayList : arraylist of elements
+     * @param array: holds the element names such as whole maps and stratergies
+     */
+    public void addElementsToList(boolean[] checkedList,ArrayList<String> arrayList,String[] array){
 
+        arrayList.clear();
+        for(int i= 0;i<checkedList.length;i++){
+            if(checkedList[i]){
+                arrayList.add(array[i]);
+            }
         }
     }
 }
