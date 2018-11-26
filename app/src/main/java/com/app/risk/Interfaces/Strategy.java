a84602ecf19424adcbac67405c7f61149d915d90
Sagar Vetal <vetal.sagar07@gmail.com>
2018-11-26 00:58:08 -0500

    Modify structure of strategy interface.

--- app/src/main/java/com/app/risk/Interfaces/Strategy.java
+++ app/src/main/java/com/app/risk/Interfaces/Strategy.java
@@ -1,7 +1,10 @@
 package com.app.risk.Interfaces;
 
+import com.app.risk.model.Country;
 import com.app.risk.model.GamePlay;
 import com.app.risk.model.Player;
+
+import java.util.ArrayList;
 
 /**
  * This interface is implemented by concrete strategy classes.
@@ -17,23 +20,30 @@ public interface Strategy {
      * It sets the no of reinforcement armies given to the player based on no of countries player owns.
      * @param gamePlay The GamePlay object.
      * @param player The Player object.
+     * @param countriesOwnedByPlayer The list of countries owned by player.
+     * @param toCountry The country where player placing the reinforcement armies.
      */
-    public void reinforcementPhase(final GamePlay gamePlay, final Player player);
+    public void reinforcementPhase(final GamePlay gamePlay, final Player player, final ArrayList<Country> countriesOwnedByPlayer, final Country toCountry);
 
     /**
      * This is attack method.
      * Attacker attacks on country according to its strategy.
      * @param gamePlay The GamePlay object.
      * @param player The Player object.
+     * @param countriesOwnedByPlayer The list of countries owned by player.
+     * @param attackingCountry The attacker country
+     * @param defendingCountry The defender country
      */
-    public void attackPhase(final GamePlay gamePlay, final Player player);
+    public void attackPhase(final GamePlay gamePlay, final Player player, final ArrayList<Country> countriesOwnedByPlayer, final Country attackingCountry, final Country defendingCountry);
 
     /**
      * This is fortification method.
      * It moves armies from one country to another country.
      * @param gamePlay The GamePlay object.
      * @param player The Player object.
+     * @param countriesOwnedByPlayer The list of countries owned by player.
+     * @param fromCountry The country from where player wants to move armies.
      */
-    public void fortificationPhase(final GamePlay gamePlay, final Player player);
+    public void fortificationPhase(final GamePlay gamePlay, final Player player, final ArrayList<Country> countriesOwnedByPlayer, final Country fromCountry);
 
 }
