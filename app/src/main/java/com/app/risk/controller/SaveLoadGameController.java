858e941137800a31d9b3019d84c2a4a3826bfdd3
akshitaangara <35636606+akshitaangara@users.noreply.github.com>
2018-11-27 13:17:27 -0500

    Javadocs

--- app/src/main/java/com/app/risk/controller/SaveLoadGameController.java
+++ app/src/main/java/com/app/risk/controller/SaveLoadGameController.java
@@ -5,20 +5,44 @@ import android.content.Context;
 import com.app.risk.model.GamePlay;
 import com.app.risk.utility.SaveLoadGame;
 
+/**
+ * Controller to save and load a game.
+ *
+ * @author Akshita Angara
+ * @version 1.0.0
+ */
 public class SaveLoadGameController {
 
     private static SaveLoadGame saveLoadGame = new SaveLoadGame();
 
+    /**
+     * Method to save the game and its state with the filename chosen by the user
+     * @param gamePlay Game object to be saved
+     * @param fileName Name of file chosen by user
+     * @param context Context of the application
+     * @return True if file is successfully written, false otherwise
+     */
     public static boolean saveGame(GamePlay gamePlay, String fileName, Context context) {
 
         return saveLoadGame.saveGame(gamePlay, fileName, context);
     }
 
+    /**
+     * Method to load the game chosen by the user for play
+     * @param fileName Name of file chosen by user to be loaded
+     * @param context Context of the application
+     * @return GamePlay object that is loaded from the file chosen by the user
+     */
     public static GamePlay loadGame(String fileName, Context context) {
 
         return saveLoadGame.loadGame(fileName, context);
     }
 
+    /**
+     * Method to return a list of all the saved games
+     * @param context Context of the application
+     * @return List of all the saved games
+     */
     public static String[] savedGamesList(Context context){
 
         return saveLoadGame.savedGamesList(context);
